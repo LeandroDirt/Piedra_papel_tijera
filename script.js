25437e9236e391f50fa419fbf0a6e69e428cc206
LeandroDirt <196249891+LeandroDirt@users.noreply.github.com>
2025-02-20 17:11:09 -0300

    Juego completado

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function getComputerChoice() {
-  let choice = Math.floor(Math.random() * 30) + 1;
-  if (choice >= 0 && choice <= 10) {
+  const COMPUTER_CHOICE = Math.floor(Math.random() * 30) + 1;
+  if (COMPUTER_CHOICE >= 1 && COMPUTER_CHOICE <= 10) {
     return "Piedra";
-  } else if (choice >= 11 && choice <= 20) {
+  } else if (COMPUTER_CHOICE >= 11 && COMPUTER_CHOICE <= 20) {
     return "Papel";
   } else {
     return "Tijera";
@@ -10,44 +10,59 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  let choice = prompt("Piedra, papel o tijera?: ");
-  if (choice.toLowerCase() === "piedra") {
+  const HUMAN_CHOICE = prompt("Piedra, papel o tijera?: ").toLowerCase();
+  if (HUMAN_CHOICE === "piedra") {
     return "Piedra";
-  } else if (choice.toLowerCase() === "papel") {
+  } else if (HUMAN_CHOICE === "papel") {
     return "Papel";
-  } else if (choice.toLowerCase() === "tijera") {
+  } else if (HUMAN_CHOICE === "tijera") {
     return "Tijera";
   } else {
+    console.log("No es una opción válida");
     return getHumanChoice();
   }
 }
 
-let computerScore = 0;
-let humanScore = 0;
-
-function playRound() {
-  let computerChoice = getComputerChoice();
-  let humanChoice = getHumanChoice();
-
+function playRound(computerChoice, humanChoice) {
   if (computerChoice === humanChoice) {
-    console.log("Empate!");
-  } else if (computerChoice === "Piedra" && humanChoice === "Papel") {
-    humanScore++;
-    console.log("Perfecto!, te llevas un punto");
+    console.log("Hay empate");
   } else if (computerChoice === "Piedra" && humanChoice === "Tijera") {
+    console.log("Perdiste, Piedra le gana a Tijera");
     computerScore++;
-    console.log("Perdiste, punto para tu adversario");
   } else if (computerChoice === "Papel" && humanChoice === "Piedra") {
+    console.log("Perdiste, Papel le gana a Piedra");
     computerScore++;
-    console.log("Perdiste, punto para tu adversario");
-  } else if (computerChoice === "Papel" && humanChoice === "Tijera") {
-    humanScore++;
-    console.log("Perfecto!, te llevas un punto");
-  } else if (computerChoice === "Tijera" && humanChoice === "Piedra") {
-    humanScore++;
-    console.log("Perfecto!, te llevas un punto");
+  } else if (computerChoice === "Tijera" && humanChoice === "Papel") {
+    console.log("Perdiste, Tijera le gana a Papel");
+    computerScore++;
   } else {
-    computerScore++;
-    console.log("Perdiste, punto para tu adversario");
+    console.log(`Ganaste, ${humanChoice} le gana a ${computerChoice}`);
+    humanScore++;
   }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+let HUMAN_SELECTION;
+let COMPUTER_SELECTION;
+
+function playGame() {
+  let contador = 0;
+
+  for (let i = 0; i <= 5; i++) {
+    if (contador === 5 && humanScore === computerScore) {
+      console.log(`Hay empate ${humanScore} a ${computerScore}`);
+    } else if (contador === 5 && humanScore > computerScore) {
+      console.log(`Ganaste ${humanScore} a ${computerScore}`);
+    } else if (contador === 5 && humanScore < computerScore) {
+      console.log(`Perdiste ${computerScore} a ${humanScore}`);
+    } else {
+      HUMAN_SELECTION = getHumanChoice();
+      COMPUTER_SELECTION = getComputerChoice();
+      playRound(COMPUTER_SELECTION, HUMAN_SELECTION);
+    }
+    contador++;
+  }
+}
+
+playGame();
